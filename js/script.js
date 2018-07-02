02e72b04f779fdd9cb798bfcf79bf8738e3c8b79
Serhiy Onischenko <39742447+Serheus@users.noreply.github.com>
2018-07-02 14:55:27 +0300

    Исправление замечаний

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-var btnOpenClose = document.querySelector('.form-btn');
-var searchForm = document.querySelector('.search-form');
-var dateArrival = searchForm.querySelector('[name=date-arrival]');
-var dateDeparture = searchForm.querySelector('[name=date-departure]');
-var quantityAdults = searchForm.querySelector('[name=quantity-adults]');
+var btnOpenClose = document.querySelector(".form-btn");
+var searchForm = document.querySelector(".search-form");
+var dateArrival = searchForm.querySelector("[name=date-arrival]");
+var dateDeparture = searchForm.querySelector("[name=date-departure]");
+var quantityAdults = searchForm.querySelector("[name=quantity-adults]");
 
-btnOpenClose.addEventListener('click', function(evt) {
+btnOpenClose.addEventListener("click", function(evt) {
   evt.preventDefault();
-  searchForm.classList.toggle('search-form-close');
+  searchForm.classList.toggle("search-form-close");
 });
 
-searchForm.addEventListener('submit', function (evt) {
+searchForm.addEventListener("submit", function (evt) {
     if (!dateArrival.value || !dateDeparture.value || !quantityAdults.value && !dateArrival.value) {
       evt.preventDefault();
-      searchForm.classList.remove('search-form-error');
+      searchForm.classList.remove("search-form-error");
       searchForm.offsetWidth = searchForm.offsetWidth;
-      searchForm.classList.add('search-form-error');
+      searchForm.classList.add("search-form-error");
     };
 });
